1dcc622202c4a031ca3e49d21acf3918684d785e
szutciek <82758495+szutciek@users.noreply.github.com>
2026-03-23 02:14:17 +0100

    Added MyAura section

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -119,6 +119,68 @@
     <section vertical>
       <ProjectDemonstration>
         <template #head>
+          <h2>MyAura Online Store</h2>
+          <p>E-COMMERCE WEBSITE WITH PAYMENT INTEGRATION AND INVENTORY SYSTEM</p>
+        </template>
+        <template #buttons>
+          <OpaqueButton @click="openUrl(`https://myaura.kanapka.eu`)">
+            <AdjacentIcon>
+              <p>Browse demo version</p>
+              <p>&nearr;</p>
+            </AdjacentIcon>
+          </OpaqueButton>
+          <TransparentButton @click="openUrl(`https://github.com/szutciek/myaura`)">
+            <AdjacentIcon>
+              <p>See source code</p>
+              <p>&nearr;</p>
+            </AdjacentIcon>
+          </TransparentButton>
+        </template>
+        <template #screenshots>
+          <section marker>
+            <ParallaxContent :progress="sections[3]?.markers[0]?.visibleProgress" velocity="1">
+              <img src="/images/myaura/home.avif" alt="" />
+              <p>Aesthetic home page with adjustable slideshow</p>
+            </ParallaxContent>
+          </section>
+          <section marker>
+            <ParallaxContent :progress="sections[3]?.markers[1]?.visibleProgress" velocity="2">
+              <FullScreenableMedia
+                text="Modern checkout form offering clients flexibility & robust feedback"
+              >
+                <img src="/images/myaura/checkout.avif" alt="" />
+              </FullScreenableMedia>
+              <p>Modern checkout form offering clients flexibility<br />& robust feedback</p>
+            </ParallaxContent>
+          </section>
+          <section marker>
+            <ParallaxContent :progress="sections[3]?.markers[2]?.visibleProgress" velocity="1">
+              <img src="/images/myaura/inpost.avif" alt="" />
+              <p>Built in InPost parcel locker selector for convenience</p>
+            </ParallaxContent>
+          </section>
+          <section marker>
+            <ParallaxContent :progress="sections[3]?.markers[3]?.visibleProgress" velocity="2">
+              <img src="/images/myaura/payment.avif" alt="" />
+              <p>Secure payment provider integration</p>
+            </ParallaxContent>
+          </section>
+          <section marker>
+            <ParallaxContent :progress="sections[3]?.markers[4]?.visibleProgress" velocity="1">
+              <FullScreenableMedia
+                text="Order confirmation & update notifications via elegant HTML emails"
+              >
+                <img src="/images/myaura/email.avif" alt="" />
+              </FullScreenableMedia>
+              <p>Order confirmation & update notifications<br />via elegant HTML emails</p>
+            </ParallaxContent>
+          </section>
+        </template>
+      </ProjectDemonstration>
+    </section>
+    <section vertical>
+      <ProjectDemonstration>
+        <template #head>
           <h2>Cafeteria Kiosk</h2>
           <p>MCDONALDS STYLE DISPLAY FOR TAKING ORDERS AT A SCHOOL</p>
         </template>
@@ -132,13 +194,13 @@
         </template>
         <template #screenshots>
           <section marker>
-            <ParallaxContent :progress="sections[3]?.markers[0]?.visibleProgress" velocity="1">
+            <ParallaxContent :progress="sections[4]?.markers[0]?.visibleProgress" velocity="1">
               <img src="/images/checkout/login.avif" alt="" />
               <p>Intuitive & fast checkout: scan QR code on phone when logged in</p>
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[3]?.markers[1]?.visibleProgress" velocity="2">
+            <ParallaxContent :progress="sections[4]?.markers[1]?.visibleProgress" velocity="2">
               <FullScreenableMedia text="Clean interface optimized for interaction via touchscreen">
                 <img src="/images/checkout/touchscreen.avif" alt="" />
               </FullScreenableMedia>
@@ -146,9 +208,9 @@
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[3]?.markers[2]?.visibleProgress" velocity="0.5">
+            <ParallaxContent :progress="sections[4]?.markers[2]?.visibleProgress" velocity="0.5">
               <FullScreenableMedia text="Satisfying animations">
-                <VideoScrollSync :progress="sections[3]?.markers[2]?.visibleProgress">
+                <VideoScrollSync :progress="sections[4]?.markers[2]?.visibleProgress">
                   <video
                     src="/videos/checkout/animations-compressed.webm"
                     muted
@@ -160,8 +222,8 @@
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[3]?.markers[3]?.visibleProgress" velocity="0.5">
-              <VideoScrollSync :progress="sections[3]?.markers[3]?.visibleProgress">
+            <ParallaxContent :progress="sections[4]?.markers[3]?.visibleProgress" velocity="0.5">
+              <VideoScrollSync :progress="sections[4]?.markers[3]?.visibleProgress">
                 <video src="/videos/checkout/panel-compressed.webm" muted playsinline></video>
               </VideoScrollSync>
               <p>Panels adjustable by touch</p>
@@ -192,27 +254,27 @@
         </template>
         <template #screenshots>
           <section marker>
-            <ParallaxContent :progress="sections[4]?.markers[0]?.visibleProgress" velocity="1">
+            <ParallaxContent :progress="sections[5]?.markers[0]?.visibleProgress" velocity="1">
               <img src="/images/roomganizer/map.avif" alt="" />
               <p>3D map layout of facility displaying availability</p>
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[4]?.markers[1]?.visibleProgress" velocity="2">
+            <ParallaxContent :progress="sections[5]?.markers[1]?.visibleProgress" velocity="2">
               <img src="/images/roomganizer/scheduler.avif" alt="" />
               <p>Customizable event scheduler</p>
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[4]?.markers[2]?.visibleProgress" velocity="1">
+            <ParallaxContent :progress="sections[5]?.markers[2]?.visibleProgress" velocity="1">
               <img src="/images/roomganizer/timeline.avif" alt="" />
               <p>Clean timeline of upcoming events</p>
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[4]?.markers[3]?.visibleProgress" velocity="0.5">
+            <ParallaxContent :progress="sections[5]?.markers[3]?.visibleProgress" velocity="0.5">
               <FullScreenableMedia text="Satisfying animations">
-                <VideoScrollSync :progress="sections[4]?.markers[3]?.visibleProgress">
+                <VideoScrollSync :progress="sections[5]?.markers[3]?.visibleProgress">
                   <video src="/videos/roomganizer/window-compressed.webm" muted playsinline></video>
                 </VideoScrollSync>
               </FullScreenableMedia>
@@ -244,13 +306,13 @@
         </template>
         <template #screenshots>
           <section marker>
-            <ParallaxContent :progress="sections[5]?.markers[0]?.visibleProgress" velocity="1">
+            <ParallaxContent :progress="sections[6]?.markers[0]?.visibleProgress" velocity="1">
               <img src="/images/zolwie/game.avif" alt="" />
               <p>A game of chasing turtles</p>
             </ParallaxContent>
           </section>
           <section marker>
-            <ParallaxContent :progress="sections[5]?.markers[1]?.visibleProgress" velocity="2">
+            <ParallaxContent :progress="sections[6]?.markers[1]?.visibleProgress" velocity="2">
               <img src="/images/zolwie/menu.avif" alt="" />
               <p>
                 Customize turtles,<br />
