e781834821fc533cb4f6373d57d0394abafbd7bc
szutciek <82758495+szutciek@users.noreply.github.com>
2026-03-25 03:30:00 +0100

    Organized images well in landing background

--- src/components/effects/TileImageGrid.vue
+++ src/components/effects/TileImageGrid.vue
@@ -3,9 +3,9 @@
     <div class="perspective" :style="perspectiveStyle">
       <div class="grid" :style="gridStyle">
         <div
-          v-for="(column, i) in arrangedItems"
+          v-for="(column, i) in arrangement"
           :key="i"
-          :style="`width: ${60 / nColumns}vw; transform: translateY(${((i % 2) + 1) * offset}px)`"
+          :style="`width: ${67 / arrangement.length}vw; transform: translateY(${((i % 2) + 1) * offset}px)`"
         >
           <img v-for="(row, j) in column" :key="j" :src="images[row]" draggable="false" />
         </div>
@@ -17,11 +17,10 @@
 <script setup>
 const props = defineProps({
   images: { type: Array, default: [] },
+  arrangement: { type: Array, default: [] },
   offset: { type: Number, default: 0 },
 })
 
-const nColumns = 6
-const nRows = 7
 const opacity = 0.1
 
 const perspective = 500
@@ -35,41 +34,14 @@ const perspectiveStyle = {
 }
 const gridStyle = ref({
   opacity: opacity,
-  gridTemplateColumns: `repeat(${nColumns}, 1fr)`,
+  gridTemplateColumns: `repeat(${props.arrangement.length}, 1fr)`,
   transform: [
-    'translate(0, -50%)',
+    'translate(0, -25%)',
     `rotateX(${rX}deg)`,
     `rotateY(${rY}deg)`,
     `rotateZ(${rZ}deg)`,
   ].join(' '),
 })
-
-const arrangedItems = ref([])
-for (let i = 0; i < nColumns; i++) {
-  arrangedItems.value.push([])
-}
-
-const sampleOrder = () => {
-  if (!props.images) return
-  const numberReps = Math.ceil((nColumns * nRows) / props.images.length)
-  const repLimit = Array(props.images.length).fill(numberReps)
-  let prevIndex = null
-  for (let i = 0; i < nColumns; i++) {
-    for (let j = 0; j < nRows; j++) {
-      const imageIndex = Math.floor(Math.random() * props.images.length)
-      const accept = repLimit[imageIndex] !== 0 && imageIndex !== prevIndex
-      if (!accept) {
-        // Leading to infinite loop
-        // j--
-        continue
-      }
-      repLimit[imageIndex]--
-      prevIndex = imageIndex
-      arrangedItems.value[i][j] = imageIndex
-    }
-  }
-}
-sampleOrder()
 </script>
 
 <style scoped>
@@ -96,7 +68,7 @@ sampleOrder()
   transform-style: flat;
   display: grid;
   gap: 10px;
-  align-items: center;
+  /* align-items: center; */
 }
 
 img {
